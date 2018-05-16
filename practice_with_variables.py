f585cb902de251204fc55eec840e4974fdf2438d
github username <github email address>
2018-05-16 11:20:19 -0400

    Completed Practice with Variables

---
+++ practice_with_variables.py
@@ -0,0 +1,13 @@
+# Assign a variable of travel_month equal to the string "January", as that is the month we would like to travel.
+travel_month = None
+travel_month = "January"
+print travel_month
+
+# Assign a variable equal to the number of weeks that we would like to travel, 3.
+number_of_weeks = None
+number_of_weeks = 3
+print number_of_weeks
+
+# Update, we're now traveling 5 weeks
+number_of_weeks = number_of_weeks + 2 #nunber_of_weeks = 5
+print number_of_weeks
