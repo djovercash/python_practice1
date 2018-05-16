0822e7b13fc301a648291eb854ca087fdca5e8d0
github username <github email address>
2018-05-16 11:15:50 -0400

    Start Practice with Python Variables

---
+++ naming_with_variables.py
@@ -0,0 +1,23 @@
+email = "art.vandelay@vandelay.com"
+website = "www.vandelay.com"
+name = "Art Vandelay"
+print email
+print website
+print name
+
+# None is a data type in Python that represents nothing.
+# So, if we do not know the type of a variable and want to have the data to the
+# variable be assigned later, we can assign that variable to None.
+None
+print type(None)
+
+address = None
+print address
+
+#Interpolate variables and strings
+print "Send an email to " + name + " at " + email +  " to say how nice it was meeting yesterday."
+
+#Change variables
+name = 'Liz Kaplan'
+email = 'liz@ka-plan.com'
+print "Send an email to " + name + " at " + email +  " to say how nice it was meeting yesterday."
