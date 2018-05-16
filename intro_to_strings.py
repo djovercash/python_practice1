dc196acdc26b0859ba30792b541edc3a68486629
github username <github email address>
2018-05-16 10:43:07 -0400

    Pythong Numbers and Booleans

--- intro_to_strings.py
+++ intro_to_strings.py
@@ -4,3 +4,12 @@ print type(3)
 print "Homer Simpson".upper()
 
 #We can operate on a datatype with the following format: [DATATYPE] [DOT] [METHOD NAME] [PARENTHESES]
+print "Homer Simpson".endswith('Simpson')
+print "Charles Montgomery Burns".endswith('Simpson')
+
+# help(str)
+#type q to exit help
+
+print "smithers".capitalize()
+
+print "hello world".title()
