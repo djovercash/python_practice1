b045e6e1ca43322430b6ca8410410121e870eb15
github username <github email address>
2018-05-16 11:05:42 -0400

    Finished Practice

---
+++ practice_with_datatypes.py
@@ -0,0 +1,20 @@
+#Capitalize all the letters of "art vandelay". Make it "ART VANDELAY"
+print "art vandelay".upper()
+
+#Capitalize the first letter of each word in "art vandelay". Make it "Art Vandelay"
+print "art vandelay".title()
+
+#Capitalize all the letters of "Ceo". Make it "CEO"
+print "Ceo".upper()
+
+#Write a method that returns a boolean for if this is a real email address: "art.vandelay@vandelay.co". Should return false
+print "art.vandelay@vandelay.co".endswith(".com")
+
+#Perform string interpolation to add "www" to "vandelay.com"
+print "www." + "vandelay.com"
+
+#Coerce string of numbers (7285553334) into an integer and add 1
+print int("7285553334") + 1
+
+#Coerce string of numbers (7285553334) into an integer and add 2
+print int("7285553334") + 2
