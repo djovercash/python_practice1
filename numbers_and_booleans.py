aead9dad4892035bf2b046c17742e6472f340fe3
github username <github email address>
2018-05-16 10:51:48 -0400

    Boolean and Number pratice

---
+++ numbers_and_booleans.py
@@ -0,0 +1,19 @@
+print (1 + 1)
+print (2 * 5)
+
+print type(10) #type int
+print type(10.2) #type float
+print type(True) #type bool
+
+print "Homer Simpson".endswith("Simpson") #True
+print "Homer Simpson".endswith("Homer") #False
+print 3 * 5 < 10 #False
+
+print "34th street".title()
+print 34 < 22
+print "34th Street" < 22
+print int("34") < 22
+print str(34) + "th Street"
+
+print bool(100)
+print bool(0)
